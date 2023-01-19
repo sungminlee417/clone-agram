f5b31799210f7b207cf8f37972b483e5a3f3b10e
Sungmin Lee <sungminlee417@gmail.com>
2023-01-19 11:53:53 -0800

    testing out more media queries

--- react-app/src/components/DirectMessagingComponents/DirectMessagingPage/DirectMessagingPage.js
+++ react-app/src/components/DirectMessagingComponents/DirectMessagingPage/DirectMessagingPage.js
@@ -24,107 +24,116 @@ const DirectMessagingPage = () => {
   }, [dispatch]);
 
   return (
-    <section className="direct-messaging-page-container">
-      <div className="direct-messaging-page-left">
-        <header>
-          <div>{currentUser.username}</div>
-          <button
-            className="direct-messaging-page-left-write"
-            onClick={() => setShowModal(true)}
-          >
-            <i className="fa-regular fa-pen-to-square"></i>
-          </button>
-        </header>
-        <div className="direct-messaging-messages">
-          {directMessagesArray.map((directMessage, i) => {
-            delete directMessage.members[currentUser.id];
-            return (
-              <NavLink
-                className="direct-messaging-message-link"
-                key={i}
-                to={`/direct/${directMessage.id}`}
-              >
-                {Object.values(directMessage.members).length > 1 ? (
-                  <div className="direct-messaging-message-link-user-profile-two">
-                    <img
-                      className="direct-messaging-message-link-user-profile-two-one"
-                      src={
-                        Object.values(directMessage.members)[0].user.profileImg
-                      }
-                      alt="user one profile"
-                    />
-                    <img
-                      className="direct-messaging-message-link-user-profile-two-two"
-                      src={
-                        Object.values(directMessage.members)[1].user.profileImg
-                      }
-                      alt="user two profile"
-                    />
-                  </div>
-                ) : (
-                  <div className="direct-messaging-message-link-user-profile-one">
-                    <img
-                      className="direct-messaging-message-link-user-profile-image"
-                      src={
-                        Object.values(directMessage.members)[0].user.profileImg
-                      }
-                      alt="user profile"
-                    />
-                  </div>
-                )}
-                <div className="direct-messaging-message-user-name-container">
-                  {Object.values(directMessage.members).map((member, i) => {
-                    if (member.user.id !== currentUser.id) {
-                      return (
-                        <div
-                          className="direct-messaging-message-user-container"
-                          key={i}
-                        >
-                          <div className="direct-messaging-message-user-name">
-                            {i === 0 && `${member.user.name}`}
-                            {Object.values(directMessage.members).length - 1 ===
-                              i &&
-                              i !== 0 &&
-                              `and ${member.user.name}`}
-                            {i <
-                              Object.values(directMessage.members).length - i &&
-                              i > 0 &&
-                              `, ${member.user.name}`}
+    <section className="direct-messaging-page-section">
+      <div className="direct-messaging-page-container">
+        <div className="direct-messaging-page-left">
+          <header>
+            <div>{currentUser.username}</div>
+            <button
+              className="direct-messaging-page-left-write"
+              onClick={() => setShowModal(true)}
+            >
+              <i className="fa-regular fa-pen-to-square"></i>
+            </button>
+          </header>
+          <div className="direct-messaging-messages">
+            {directMessagesArray.map((directMessage, i) => {
+              delete directMessage.members[currentUser.id];
+              return (
+                <NavLink
+                  className="direct-messaging-message-link"
+                  key={i}
+                  to={`/direct/${directMessage.id}`}
+                >
+                  {Object.values(directMessage.members).length > 1 ? (
+                    <div className="direct-messaging-message-link-user-profile-two">
+                      <img
+                        className="direct-messaging-message-link-user-profile-two-one"
+                        src={
+                          Object.values(directMessage.members)[0].user
+                            .profileImg
+                        }
+                        alt="user one profile"
+                      />
+                      <img
+                        className="direct-messaging-message-link-user-profile-two-two"
+                        src={
+                          Object.values(directMessage.members)[1].user
+                            .profileImg
+                        }
+                        alt="user two profile"
+                      />
+                    </div>
+                  ) : (
+                    <div className="direct-messaging-message-link-user-profile-one">
+                      <img
+                        className="direct-messaging-message-link-user-profile-image"
+                        src={
+                          Object.values(directMessage.members)[0].user
+                            .profileImg
+                        }
+                        alt="user profile"
+                      />
+                    </div>
+                  )}
+                  <div className="direct-messaging-message-user-name-container">
+                    {Object.values(directMessage.members).map((member, i) => {
+                      if (member.user.id !== currentUser.id) {
+                        return (
+                          <div
+                            className="direct-messaging-message-user-container"
+                            key={i}
+                          >
+                            <div className="direct-messaging-message-user-name">
+                              {i === 0 && `${member.user.name}`}
+                              {Object.values(directMessage.members).length -
+                                1 ===
+                                i &&
+                                i !== 0 &&
+                                `and ${member.user.name}`}
+                              {i <
+                                Object.values(directMessage.members).length -
+                                  i &&
+                                i > 0 &&
+                                `, ${member.user.name}`}
+                            </div>
                           </div>
-                        </div>
-                      );
-                    } else {
-                      return null;
-                    }
-                  })}
-                </div>
-              </NavLink>
-            );
-          })}
+                        );
+                      } else {
+                        return null;
+                      }
+                    })}
+                  </div>
+                </NavLink>
+              );
+            })}
+          </div>
         </div>
-      </div>
-      <div className="direct-messaging-page-right">
-        <Switch>
-          <Route exact path="/direct/inbox">
-            <div className="direct-messaging-direct-inbox-default">
-              <img
-                src="https://cloneagram.s3.us-west-1.amazonaws.com/svgexport-11.svg"
-                alt="create message"
-              />
-              <header>Your Messages</header>
-              <div>Send private photos and messages to a friend or group.</div>
-              <button
-                className="direct-messaging-direct-inbox-default-button"
-                onClick={() => setShowModal(true)}
-              >
-                Send Message
-              </button>
-            </div>
-          </Route>
-          <Route exact path="/direct/:directMessageId">
-            <DisplayMessages />
-          </Route>
-        </Switch>
+        <div className="direct-messaging-page-right">
+          <Switch>
+            <Route exact path="/direct/inbox">
+              <div className="direct-messaging-direct-inbox-default">
+                <img
+                  src="https://cloneagram.s3.us-west-1.amazonaws.com/svgexport-11.svg"
+                  alt="create message"
+                />
+                <header>Your Messages</header>
+                <div>
+                  Send private photos and messages to a friend or group.
+                </div>
+                <button
+                  className="direct-messaging-direct-inbox-default-button"
+                  onClick={() => setShowModal(true)}
+                >
+                  Send Message
+                </button>
+              </div>
+            </Route>
+            <Route exact path="/direct/:directMessageId">
+              <DisplayMessages />
+            </Route>
+          </Switch>
+        </div>
       </div>
       {showModal && (
         <Modal onClose={() => setShowModal(false)} showModal={showModal}>
